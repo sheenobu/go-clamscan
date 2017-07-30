505f065f166d5c6ff21e0ae54f54ff85b5f7e07e
Sheena Artrip <sheenobu@fb.com>
2017-07-30 00:31:35 -0700

    Fix generation of test db

--- generate.go
+++ generate.go
@@ -2,5 +2,5 @@ package clamscan
 
 //TODO: move these to go-based tooling or remove them completely
 
-//go:generate /bin/sh -c "echo -n 'Trojan.Win32.Emold.A:1:*:' > customsig.ndb"
-//go:generate /bin/sh -c "cat testfile | sigtool --hex-dump | head -c 2048 >> customsig.ndb"
+//go:generate /bin/bash -c "echo -n 'Trojan.Win32.Emold.A:0:*:' > customsig.ndb"
+//go:generate /bin/bash -c "cat testfile | sigtool --hex-dump | head -c 2048 >> customsig.ndb"
